3bb5fc6c4b74502979a1e226212037ca5f5700e6
worachatsun <worachat.sun@mail.kmutt.ac.th>
2016-10-17 10:17:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 var express = require('./config/express');
 var app = express();
